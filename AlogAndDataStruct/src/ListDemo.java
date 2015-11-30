e61f9221504040f860493e048c041a738a18d218
Tikitoo <codes0306@gmail.com>
2015-11-30 21:52:43 +0800

    init repo

---
+++ AlogAndDataStruct/src/ListDemo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Tikitoo on 2015/11/30.
+ */
+public class ListDemo {
+    private static List<String> mList;
+    public static void main(String[] args) {
+        initData();
+        for(String str : initData()) {
+            System.out.println(str);
+        }
+    }
+
+    private static List<String> initData() {
+        mList = new ArrayList<String>();
+
+        for (int i = 0; i < 30; i++) {
+            mList.add("x= i " + i);
+        }
+        return mList/* != null ? mList : new ArrayList<String>()*/;
+    }
+}
